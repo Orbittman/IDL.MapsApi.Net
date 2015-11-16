7d35df4c3766f6908f0d1d9613aa44d36339a898
Tim Barton <tim@vouchercloud.com>
2015-11-16 16:57:17 +0000

    Adding missed file

---
+++ IDL.MapsApi.Net/GeoLocationProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+using IDL.MapsApi.Net.Client;
+
+namespace IDL.MapsApi.Net
+{
+    public class GeoLocationProvider : IGeoLocationProvider
+    {
+        private readonly IApiClient _client;
+
+        public GeoLocationProvider(IApiClient client = null)
+        {
+            _client = client ?? new ApiClient();
+        }
+
+        public Task<TResponse> GetAsync<TResponse>(IRequest<TResponse> request)
+            where TResponse : class, new() 
+        {
+            return _client.GetAsync(request);
+        }
+    }
+}
