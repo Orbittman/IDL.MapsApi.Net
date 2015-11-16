e038f54b84277fbaf3b4a0f60351d3200c5fb980
Tim Barton <github@trashcompactor.co.uk>
2015-11-16 15:23:23 +0000

    Added unit test build step

--- build.bat
+++ build.bat
@@ -13,7 +13,19 @@ call %NuGet% restore IDL.MapsApi.Net.Tests\packages.config -OutputDirectory %cd%
 
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild IDL.MapsApi.Net.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if not "%errorlevel%"=="0" goto failure
+
+REM Unit tests
+%GallioEcho% IDL.MapsApi.Net.Tests\bin\%config%\IDL.MapsApi.Net.Tests.dll
+if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir Build
 call %nuget% pack "IDL.MapsApi.Net\IDL.MapsApi.Net.csproj" -symbols -o Build -p Configuration=%config% %version%
+if not "%errorlevel%"=="0" goto failure
+
+:success
+exit 0
+
+:failure
+exit -1
