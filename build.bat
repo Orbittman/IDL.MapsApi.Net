0061933121de7114953850311f36bd3db0f733f0
Tim Barton <github@trashcompactor.co.uk>
2015-11-16 14:58:11 +0000

    Correcting the build config project file names

--- build.bat
+++ build.bat
@@ -12,8 +12,8 @@ REM Package restore
 call %NuGet% restore IDL.MapsApi.Net\IDL.MapsApi.Net.Tests\packages.config -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild MapBox.Net.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild IDL.MapsApi.Net.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
-call %nuget% pack "IDL.MapsApi.Net\IDL.MapsApi.Net\MapBox.Net.csproj" -symbols -o Build -p Configuration=%config% %version%
+call %nuget% pack "IDL.MapsApi.Net\IDL.MapsApi.Net\IDL.MapsApi.Net.csproj" -symbols -o Build -p Configuration=%config% %version%
